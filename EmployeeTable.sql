cecb0629ffbe68436fdc790deb5a9134b6f6f15c
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-09 17:15:38 +0200

    Employee table

---
+++ EmployeeTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Employees (
+	id INT,
+	name VARCHAR(20),
+	surname VARCHAR(30),
+	age NUMERIC(2),
+	address VARCHAR(100),
+	birth_date DATE,
+	salary DECIMAL(18,2),
+	active CHAR(3)
+);

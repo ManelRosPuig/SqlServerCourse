f94f28d8b8b812e75598c966deef904ca8a05240
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-09 16:32:36 +0200

    Number datatypes

--- Datatypes.sql
+++ Datatypes.sql
@@ -23,3 +23,30 @@ NCHAR
 NVARCHAR
 
 --------------------------------------------
+
+--------------------------------------------
+-- NUMBERS
+--------------------------------------------
+
+-- Store 2 values: 1 or 0.
+BIT
+
+-- Store values from -2.147.483.648 to 2.147.483.647.
+INT
+
+-- Store values from -9.223.372.036.854.775.808 to 9.223.372.036.854.775.807.
+BIGINT
+
+-- Store values from -10^38 + 1 to 10^38 -1.
+DECIMAL
+
+-- Same as DECIMAL.
+NUMERIC
+
+-- Same as BIGINT.
+MONEY
+
+-- Store values from -1.79E + 308 to 1.79E + 308.
+FLOAT
+
+--------------------------------------------

0f7105884903cd095049614efe96a3d274fdfc37
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-09 16:32:12 +0200

    Text datatypes

---
+++ Datatypes.sql
@@ -0,0 +1,25 @@
+--------------------------------------------
+-- TEXT
+--------------------------------------------
+
+-- Store datatypes of a fixed width.
+-- It can be both characters or numbers.
+CHAR
+
+-- Store datatypes of a non-fixed width.
+VARCHAR
+
+-- Store text datatypes.
+TEXT
+
+-- Store datatypes of a fixed width.
+-- If the characters are not 100% filled, 
+-- it reuses those non-used free spaces.
+NCHAR
+
+-- Store datatypes of a non-fixed width.
+-- If the characters are not 100% filled, 
+-- it reuses those non-used free spaces.
+NVARCHAR
+
+--------------------------------------------
--- FirstQuery.sql
+++ FirstQuery.sql
@@ -1,4 +1,4 @@
-USE test;
+USE main;
 
 CREATE DATABASE test2;
 DROP DATABASE IF EXISTS test2;

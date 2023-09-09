ab97e77357c4672eb518290d2f7e5595bdcf9d40
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-09 17:23:01 +0200

    Insert and Select

--- EmployeeTable.sql
+++ EmployeeTable.sql
@@ -11,3 +11,18 @@ CREATE TABLE Employees (
 
 -- See information about one table.
 EXEC sp_help employees;
+
+INSERT INTO Employees VALUES(
+	1, 'Manel', 'Ros', 19,
+	'Somewhere, 28, 08036, Barcelona, Spain',
+	'10-10-2003', 27000, 'YES'
+);
+
+INSERT INTO Employees VALUES(
+	2, 'David', 'Ros', 18,
+	'Somewhere, 28, 08036, Barcelona, Spain',
+	'08-06-2005', 17000, 'NO'
+);
+
+SELECT *
+FROM Employees;
